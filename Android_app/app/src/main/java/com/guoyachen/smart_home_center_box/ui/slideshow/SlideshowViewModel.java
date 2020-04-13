68377ece358bed13e2fc8e0c565d59641c2e3fea
HarryGuo <42864587+HarryG55@users.noreply.github.com>
2020-04-13 22:06:59 +0800

    Update for Android
    This is a simple structure for Android(have a lot of bug)

---
+++ Android_app/app/src/main/java/com/guoyachen/smart_home_center_box/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.guoyachen.smart_home_center_box.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
